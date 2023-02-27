bb84a40e5edc7a4f7f1ab18583b8ec7e73627c4d
maks <maks1makrov@gmail.com>
2023-02-27 20:48:01 +0300

    basics_01

---
+++ basics/basics_01.py
@@ -0,0 +1,13 @@
+# Определить переменные всех 4 базовых типов и вывести их на экран
+
+my_int = 21
+print(my_int)
+
+my_float = 21.5
+print(my_float)
+
+my_str = "asdfghjkl"
+print(my_str)
+
+my_bool = True
+print(my_bool)
