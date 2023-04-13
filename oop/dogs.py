0d0fe72d8df31fb244617d9e143b97c1a6a46126
maks <maks1makrov@gmail.com>
2023-04-13 19:37:11 +0300

    oop - 7

--- oop/dogs.py
+++ oop/dogs.py
@@ -2,12 +2,20 @@ class Dog:
     pet = 'angry'
 
     def __init__(self, height, weight, name, age, master, adres='Minsk'):
-        self.height = height
-        self.weight = weight
-        self.name = name
-        self.age = age
+        self.__height = height
+        self.__weight = weight
+        self.__name = name
+        self.__age = age
         self.__master = master
         self.__adres = adres
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, new_height):
+        self.__height = new_height
 
     def jump(self):
         print("jump")
@@ -16,20 +24,16 @@ class Dog:
         print("run")
 
     def bark(self):
-        print(f'bark from {self.name}')
+        print(f'bark from {self.__name}')
 
-    def change_name(self, new_name):
-        self.name = new_name
 
-    def get_master(self):
-        print(self.__master)
-        return self.__master
 
-    def get_adres(self):
-        return self.__adres
-
-    def set_adres(self, new_adres):
-        self.__adres = new_adres
+    #
+    # def get_adres(self):
+    #     return self.__adres
+    #
+    # def set_adres(self, new_adres):
+    #     self.__adres = new_adres
 
 
 dog_1 = Dog(name="Bob", age=5, height=20, weight=3, master='Boris')
@@ -37,6 +41,9 @@ print(dog_1.__dict__)
 dog_1.bark()
 # dog_1.__name = 'testBob'
 print(dog_1._Dog__master)
+print(dog_1.height)
+dog_1.height = 50
+print(dog_1.height)
 dog_1.age = 10
 Dog.pet = 'not angry'
 dog_1.change_name("Bobik")
